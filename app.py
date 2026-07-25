1a551e8a05273a1d6709f3c8fcd5d6d67f212abc
The-Rafeicious <joshuahillburgoyne@gmail.com>
2026-07-25 22:09:06 +0100

    changed app to allow sessions

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from math import ceil
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
 from config import BELT_SPEEDS
 from smelting_form_processing import smelting_form_base
 
 app = Flask(__name__)
+app.secret_key = "super_secret_factorio_key"
 
 @app.route("/")
 def MainMenu():
@@ -15,11 +16,22 @@ def smelting_form():
     if request.method == "POST":
         raw_data = request.form
 
-        final_data = smelting_form_base(raw_data)
+        session['results'] = smelting_form_base(raw_data)
 
-        return render_template("results_HTML.html", results=final_data)
+        return redirect(url_for('smelting_results'))
 
     return render_template("smelting_form_HTML.html")
+
+@app.route("/smelting-results", methods=["GET", "POST"])
+def smelting_results():
+    final_data = session.get("results")
+
+    if not final_data:
+        return redirect(url_for('MainMenu'))
+
+    return render_template("results_HTML.html", results=final_data)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
